59570bf9e1ae73183019ebb1b6c9e418563a3eb7
Saurav Suman <sauravsrivastav9876@gmail.com>
2024-10-20 10:19:25 +0530

    theme updated

--- script.js
+++ script.js
@@ -1,109 +1,248 @@
-console.log("Welcome to Spotify");
-
-// Initialize the Variables
-let songIndex = 0;
-let audioElement = new Audio('songs/1.mp3');
-let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar');
-let gif = document.getElementById('gif');
-let masterSongName = document.getElementById('masterSongName');
-let songItems = Array.from(document.getElementsByClassName('songItem'));
-
-let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
-]
-
-songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
-})
- 
-
-// Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
+var caard =[
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+    {songName :"Badmas - dil",  artist:"Arijit Singh",duration: "3.48" ,url :"m1.mp3",img:"img1.jpeg"},
+    {songName :"Tere Bina",artist:"Arijit Singh", duration :"2.56",url :"m2.mp3",img:"img2.jpg"} ,
+    {songName :"Arjan Vailly ",artist :"Bhupinder Babbal",duration :"3.10" ,url :"m3.mp3" ,img:"https://play-lh.googleusercontent.com/1JA3ZIZ05yDa-O9yK1dhRNfR0C7JvUw6RZ1VrteLc5-l6j5GHaNrGmi1BFcil-RvN5Y=w540-h302-rw"},
+    
+    
+    {songName:"blue-eyes", artist:"honey sing", duration:"2.57" ,url :"m4.mp3",img:"https://m.media-amazon.com/images/M/MV5BMjUxYmNmYjUtNmEwMy00ZGY4LTliMDgtODllNGU5ZTE5ZmI0XkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_.jpg"},
+];
+var audio = new Audio();
+var selectedSong=0;
+function  displaySongs(){
+    var cluster="";
+    caard.forEach(function(element,index) {
+        cluster+= `<div class="card" id= ${index}>
+        <img src="${element.img}" alt="">
+        <div class="song-name">${element.songName}</div>
+        <div class="artist-name">${element.artist}</div>
+        <div class="song-">${element.duration}</div>
+    </div>`
+      });
+      document.querySelector(".content").innerHTML=cluster;
+      
+      
     }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
+    displaySongs();
+    if(audio.paused){
+        document.querySelector(".play").style.display="block";
+        document.querySelector(".pause").style.display="none";
+        
     }
-})
-// Listen to Events
-audioElement.addEventListener('timeupdate', ()=>{ 
-    // Update Seekbar
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
-    myProgressBar.value = progress;
+    else if(! audio.paused){
+    document.querySelector(".play").style.display="none";
+    document.querySelector(".pause").style.display="block";
+    }
+    function songplay(){
+        var s=document.querySelector(".content")
+        var poster= "";
+        s.addEventListener("click",function(dets){
+            selectedSong=dets.target.id;
+            var songurl =caard[selectedSong].url
+            audio.src=songurl;
+            audio.play();
+            var k= `<div class="librarylogo flex">
+            
+              <img src="${caard[selectedSong].img}" alt="">
+           
+        </div>`;
+            
+    // document.querySelector(".library").style.backgroundColor="red";
+    document.querySelector(".library").innerHTML=k
+    
+
 })
 
-myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
-})
-
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-        element.classList.remove('fa-pause-circle');
-        element.classList.add('fa-play-circle');
-    })
 }
-
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
-        makeAllPlays();
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-    })
+songplay();
+var playBtn= document.querySelector(".play");
+playBtn.addEventListener("click",function(dets){
+    if(audio.paused==true){
+        audio.src=caard[selectedSong].url;
+        audio.play();
+        var t= `<div class="librarylogo flex">
+            
+              <img src="${caard[selectedSong].img}" alt="">
+           
+        </div>`;
+        document.querySelector(".library").innerHTML=t
+        document.querySelector(".pause").style.display="block";
+        document.querySelector(".play").style.display="none";
+        
+        
+    }
+    // else{
+    //     audio.pause();
+    // }
 })
-
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
-        songIndex = 0
+var pauseBtn= document.querySelector(".pause");
+pauseBtn.addEventListener("click",function(dets){
+    if(audio.paused==false){
+        audio.src=caard[selectedSong].url;
+        audio.pause();
+        var t= `<div class="librarylogo flex">
+            
+              <img src="${caard[selectedSong].img}" alt="">
+           
+        </div>`;
+        document.querySelector(".library").innerHTML=t
+        document.querySelector(".pause").style.display="none";
+        document.querySelector(".play").style.display="block";
+        
+        
     }
-    else{
-        songIndex += 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
-
+    // else{
+    //     audio.pause();
+    // }
 })
-
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
+var nextBtn =document.querySelector(".forword");
+nextBtn.addEventListener("click",function(dets) {
+    if(selectedSong>-1 && selectedSong<caard.length - 1){
+    selectedSong++;
+    
+    audio.src=caard[selectedSong].url;
+    audio.play();
+    var j= `<div class="librarylogo flex">
+            
+              <img src="${caard[selectedSong].img}" alt="">
+           
+        </div>`;
+    document.querySelector(".library").innerHTML=j
     }
-    else{
-        songIndex -= 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
+    // if(selectedSong >= caard.length -1 ) {
+    //     nextBtn.style.opacity = "0.5";
+    // }else if(selectedSong <= caard.length -1 ){
+        
+    //     prevBtn.style.opacity = "1";
+    // }
+    
 })
+var prevBtn =document.querySelector(".pre");
+prevBtn.addEventListener("click",function(dets) {
+    if(selectedSong >0){
+        selectedSong--;
+        audio.src=caard[selectedSong].url;
+        audio.play();
+       
+        var k1= `<div class="librarylogo flex">
+            
+              <img src="${caard[selectedSong].img}" alt="">
+           
+        </div>`;
+        document.querySelector(".library").innerHTML=k1
+    }
+    // if(selectedSong <=0 ){
+    //     prevBtn.style.opacity = "0.5";
+        
+    // }else{
+        
+    //     prevBtn.style.opacity = "1";
+    // }
+})
+var progressBar = document.getElementById("progressBar");
+var timeDisplay = document.getElementById("duration");
+audio.addEventListener("timeupdate", function() {
+    var currentTime = audio.currentTime;
+    var duration = audio.duration;
+    var progress = (currentTime / duration) * 100;
+    progressBar.value = progress;
+
+
+    var currentMinutes = Math.floor(currentTime / 60);
+  var currentSeconds = Math.floor(currentTime % 60);
+  var durationMinutes = Math.floor(duration / 60);
+  var durationSeconds = Math.floor(duration % 60);
+  timeDisplay.innerText = currentMinutes + ":" + (currentSeconds < 10 ? "0" : "") + currentSeconds + " / " +
+                          durationMinutes + ":" + (durationSeconds < 10 ? "0" : "") + durationSeconds;
+  });
+  
+  // Skip forward when the progress bar is clicked
+  progressBar.addEventListener("click", function(e) {
+    var progressBarRect = progressBar.getBoundingClientRect();
+    var clickX = e.clientX - progressBarRect.left;
+    var progressBarWidth = progressBarRect.width;
+    var progress = (clickX / progressBarWidth) * 100;
+    audio.currentTime = (progress / 100) * audio.duration;
+  });
+
+
+  //serch 
+  var serch= document.querySelector(".Serch-suj");
+  var ser =document.querySelector("#ser");
+  var semain =document.querySelector("#semain");
+  semain.addEventListener("click", function(){
+    ser.style.display="block";
+  })
+
+  ser.addEventListener("focus",function(){
+    serch.style.display="block";
+    document.querySelector("#body").style.backgroundColor="black"
+  })
+  ser.addEventListener("blur",function(){
+    serch.style.display="none";
+  })
